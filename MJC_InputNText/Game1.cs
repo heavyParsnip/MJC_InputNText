1e3c7f0c14a86b4529774f8b33abba805a0a93d3
Matthew Camera <mjc9536@rit.edu>
2019-09-11 12:17:19 -0400

    Initial commit

---
+++ MJC_InputNText/Game1.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MJC_InputNText
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Texture2D sampleTexture;
+        Vector2 sampleVec;
+        Vector2 textVec;
+        Vector2 vectorVec;
+        SpriteFont sampleFont;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            //Modifying window size
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 960;
+            graphics.ApplyChanges();
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            sampleVec = new Vector2(20, 80);
+            textVec = new Vector2(20, 20);
+            vectorVec = new Vector2(20, 60);
+            sampleTexture = Content.Load<Texture2D>("bones");
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            sampleFont = Content.Load<SpriteFont>("SampleText18");
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            ProcessInput();
+
+            
+
+
+
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.MediumPurple);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            spriteBatch.Draw(sampleTexture, sampleVec, Color.White);
+            spriteBatch.DrawString(sampleFont, "Skelometer: boneless", textVec, Color.Black);
+            spriteBatch.DrawString(sampleFont, $"{sampleVec}", vectorVec, Color.Black);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        /// <summary>
+        /// Method for processing keyboard input
+        /// </summary>
+        protected void ProcessInput()
+        {
+            KeyboardState kbState = Keyboard.GetState();
+
+            if (kbState.IsKeyDown(Keys.W) == true)
+            {
+                if (kbState.IsKeyDown(Keys.LeftShift) == true)
+                {
+                    sampleVec.Y -= 10.0f;
+                }
+                else
+                {
+                    sampleVec.Y -= 5.0f;
+                }
+            }
+            if (kbState.IsKeyDown(Keys.A) == true)
+            {
+                if (kbState.IsKeyDown(Keys.LeftShift) == true)
+                {
+                    sampleVec.X -= 10.0f;
+                }
+                else
+                {
+                    sampleVec.X -= 5.0f;
+                }
+            }
+            if (kbState.IsKeyDown(Keys.S) == true)
+            {
+                if (kbState.IsKeyDown(Keys.LeftShift) == true)
+                {
+                    sampleVec.Y += 10.0f;
+                }
+                else
+                {
+                    sampleVec.Y += 5.0f;
+                }
+            }
+            if (kbState.IsKeyDown(Keys.D) == true)
+            {
+                if (kbState.IsKeyDown(Keys.LeftShift) == true)
+                {
+                    sampleVec.X += 10.0f;
+                }
+                else
+                {
+                    sampleVec.X += 5.0f;
+                }
+            }
+        }
+    }
+}
